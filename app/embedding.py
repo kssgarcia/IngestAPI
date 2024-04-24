feecdae30248461d63d73435ae674cf3f3b541d5
LordO54 <yilberlopez2000@hotmail.com>
2024-04-24 18:52:00 -0500

    busqueda vectorial coincidente con modelo de datos

---
+++ app/embedding.py
@@ -0,0 +1,122 @@
+from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from mongo_client import get_mongo_client
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+import os
+import logging
+
+load_dotenv()
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#prueba de busqueda
+# Modelo de solicitud para el endpoint /vs
+class VectorSearchRequest(BaseModel):
+    query: str
+    index: str  # Índice de búsqueda en la base de datos
+
+
+
+# Modelo de respuesta para el endpoint /vs
+class VectorSearchResponse(BaseModel):
+    results: List[str]
+
+class foodDocument(BaseModel):
+    nombre:str
+    ingredientes: List[str]
+
+class foodResponse(BaseModel):
+    food: List[foodDocument]
+
+#funcion para realizar embedding de busqueda
+def generateEmbedding(text: str) -> list[float]:
+
+    import requests
+    #jina_958f2f6f4fbb463b80367d493c9a875fRRylBWRxDmvDR6OceXRUDIwKmsNS
+    url = 'https://api.jina.ai/v1/embeddings'
+
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer jina_958f2f6f4fbb463b80367d493c9a875fRRylBWRxDmvDR6OceXRUDIwKmsNS'
+    }
+
+    data = {
+    'input': [text],
+    'model': 'jina-embeddings-v2-base-es'
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code != 200:
+        raise ValueError(f"Request failed with status code {response.status_code}: {response.text}")
+    
+    return response.json()["data"][0]["embedding"]
+
+
+#Se crean funciones para optimizacion de uso de modelo de embedding.
+async def get_embedding_from_db(query: str, db: MongoClient) -> list[float]:
+    logger.info("buscando")
+    collection = db['embeddings']
+    result = collection.find_one({"query": query})
+    if result:
+        return result["embedding"]
+    else:
+        return None
+
+async def save_embedding_to_db(query: str, embedding: list[float], db: MongoClient):
+    logger.info("guardando")
+    collection = db['embeddings']
+    collection.insert_one({"query": query, "embedding": embedding})
+
+# Función para realizar la búsqueda vectorial en la base de datos
+async def perform_vector_search(query: str, index: str) -> List[str]:
+    client = await get_mongo_client()
+    DB_NAME = os.getenv("MONGO_DB_NAME")
+    db = client[DB_NAME]
+    
+    logger.info("a ver si existe")
+    existing_embedding = await get_embedding_from_db(query, db)
+    if existing_embedding:
+        # Si el embedding ya existe en la base de datos, no es necesario generar uno nuevo
+        embedding = existing_embedding
+        logger.info("sí habia uno")
+    else:
+        # Si el embedding no existe, genera uno nuevo
+        logger.info("No habia así que voy a generar")
+        embedding = generateEmbedding(query)
+        # Guarda el nuevo embedding en la base de datos para futuras consultas
+        logger.info("ahora lo guardo")
+        await save_embedding_to_db(query, embedding, db)
+
+    logger.info("a que vector voy")
+    collection_namse = None
+    if index == "nombreIngreEmbedding":
+        collection_name = "ingredientes"
+    elif index == "nombreEmbedding":
+        collection_name = "platos"
+    else:
+        raise HTTPException(status_code=500, detail="El índice no es válido")
+    
+    
+    logger.info("ejecutando busqueda")
+    collection = db[collection_name]
+    results = collection.aggregate([
+        {"$vectorSearch": {
+            "queryVector": generateEmbedding(query),
+            "path": index,
+            "numCandidates": 100,
+            "limit": 3,
+            "index": index,
+        }}
+    ])
+
+    response_items = []
+    for document in results:
+        response_items.append(foodDocument(nombre=document["nombre"], ingredientes=document["ingredientes"]))  # Reemplaza 'nombre' por el campo real de tu documento
+    logger.info("mostrando resultados de busqueda")
+    print(response_items)
+    return response_items
--- app/main.py
+++ app/main.py
@@ -1,9 +1,45 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from model.model import predict
 from PIL import Image
 import io
+import logging
+import os
+from mongo_client import get_mongo_client
+from embedding import perform_vector_search
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from typing import List
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()  # Carga las variables de entorno desde .env
 
 app = FastAPI()
+
+class Prediction(BaseModel):
+    prediction: str
+    prob: str
+
+class PredictionResponse(BaseModel):
+    predictions: List[Prediction]
+
+class foodDocument(BaseModel):
+    nombre:str
+    ingredientes: List[str]
+class foodResponse(BaseModel):
+    food: List[List[foodDocument]]
+
+# Modelo de solicitud para el endpoint /vs
+class VectorSearchRequest(BaseModel):
+    query: str
+    index: str  # Índice de búsqueda en la base de datos
+
+
+# Modelo de respuesta para el endpoint /vs
+class VectorSearchResponse(BaseModel):
+    results: List[str]
 
 @app.get("/")
 def home():
@@ -11,7 +47,53 @@ def home():
 
 @app.post('/file/')
 async def prediction(file: UploadFile = File(...), x: float = Form(0), y: float = Form(0), width: float = Form(0), height: float = Form(0)):
-    contents = await file.read()
-    img = Image.open(io.BytesIO(contents))
-    results = predict(img, x, y, width, height)
-    return results
+    logger.info("Starting the predict function")
+    try:
+        contents = await file.read()
+
+        #Content verifying middleware
+        if not contents:
+            raise HTTPException(status_code=400, detail="The file is empty")
+
+        img = Image.open(io.BytesIO(contents))
+
+        #Compatible format middleware
+        if img.format.lower() not in ["jpeg", "jpg", "png"]:
+            raise HTTPException(status_code=400, detail="Invalid image format")
+
+        results = predict(img, x, y, width, height)
+
+        #Load mongo client
+        client = await get_mongo_client()
+        DB_NAME = os.getenv("MONGO_DB_NAME")
+        db = client[DB_NAME]
+        collection=db.platos
+
+        # Insert predictions as a single document into MongoDB
+        db.predictions.insert_one(results.dict())
+        print(results.dict())
+        logger.info("Finished the predict function")
+
+        #Vector search
+        foods=[] #5 listas de 3 documentos
+        for prediction in results.dict().get("predictions"):
+            foods.append(await perform_vector_search(query=prediction["prediction"], index="nombreEmbedding"))
+        print(foods)
+        logger.info("sacando response")
+        return foods
+    except HTTPException as e:
+        logger.error(f"HTTPException in predict function: {str(e.detail)}")
+        raise
+    except Exception as e:
+        logger.error(f"Error in predict function: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+  # Endpoint para la búsqueda vectorial
+@app.post("/vs", response_model=VectorSearchResponse)
+async def vector_search(req: VectorSearchRequest):
+    try:
+        results = await perform_vector_search(req.query, req.index)
+        return {"results": results}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
