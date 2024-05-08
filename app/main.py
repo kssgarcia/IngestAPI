81ffe129c6f5d6d76ddead75480a9f630e64b776
Kssgarcia <kssgarcia@outlook.com>
2024-05-08 16:32:30 -0500

    chore: Update model loading and file paths in main.py

--- app/main.py
+++ app/main.py
@@ -1,47 +1,56 @@
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.datastructures import UploadFile
+from pydantic import BaseModel, Field
 from llama_cpp import Llama
-from model.model import predict
 from PIL import Image, UnidentifiedImageError
+from pathlib import Path
+from torchvision.transforms.functional import crop
+from torchvision import transforms
+from ultralytics import YOLO
 import io
 
 app = FastAPI()
 
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
 # Initialize the model globally and set it to None initially
 llm = None
 
-@app.on_event("startup")
-async def load_model():
-    global llm
-    try:
-        # Load the model once when the server starts
-        llm = Llama(
-            model_path="model/Phi-3-mini-128k-instruct.IQ1_S.gguf",
-            chat_format="llama-2",
-            n_gpu_layers=-1
-        )
-        print("Model loaded successfully.")
-    except Exception as e:
-        print(f"Failed to load model: {str(e)}")
-        # Raising an exception here will stop the app from starting if the model cannot be loaded
-        raise RuntimeError("Failed to load the model at startup")
+class LlamaPredictionRequest(BaseModel):
+    prompt: str
+
+# @app.on_event("startup")
+# async def load_model():
+#     global llm
+#     try:
+#         # Load the model once when the server starts
+#         llm = Llama(
+#             model_path="model/Phi-3-mini-128k-instruct.IQ1_S.gguf",
+#             chat_format="llama-2",
+#             n_gpu_layers=-1
+#         )
+#         print("Model loaded successfully.")
+#     except Exception as e:
+#         print(f"Failed to load model: {str(e)}")
+#         raise RuntimeError("Failed to load the model at startup")
 
 @app.get("/")
 def home():
     return {"Health": "OK"}
 
 @app.post('/llama/')
-async def prediction_llama(prompt: str):
+async def llama_prediction(request: LlamaPredictionRequest):
     print('Predicting')
     try:
         if llm is None:
             raise HTTPException(status_code=503, detail="Model is not loaded")
-        output = llm(prompt, max_tokens=1024, echo=False)
-        return output['choices'][0]['text']
+        # output = llm(request.prompt, max_tokens=1024, echo=False)
+        # return output['choices'][0]['text']
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred during llama prediction: {str(e)}")
 
 @app.post('/file/')
-async def prediction(file: UploadFile = File(...), x: float = Form(0), y: float = Form(0), width: float = Form(0), height: float = Form(0)):
+async def food_prediction(file: UploadFile = File(...), x: float = Form(0), y: float = Form(0), width: float = Form(0), height: float = Form(0)):
     # Check the file type
     if not file.content_type.startswith('image/'):
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
@@ -55,7 +64,25 @@ async def prediction(file: UploadFile = File(...), x: float = Form(0), y: float 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred while processing the image: {str(e)}")
     try:
-        results = predict(img, x, y, width, height)
-        return results
+        if x + y + width + height > 0:
+            left = int(x - width / 2)
+            top = int(y - height / 2)
+            image = crop(img, top=top, left=left, height=int(height), width=int(width))
+            image = transforms.ToTensor()(image).unsqueeze(0)  # Convert to tensor
+        else:
+            transform = transforms.Compose([
+                transforms.Resize((640, 640)),  # Resize the image
+                transforms.ToTensor(),  # Convert the image to a PyTorch tensor
+            ])
+            image = transform(img).unsqueeze(0)
+        
+        model = YOLO('models/scrap100.pt')
+        result = model(image)
+        
+        # Assuming the result structure is as expected
+        names = result[0].names
+        probs = result[0].probs.top5
+        conf = result[0].probs.top5conf.tolist()
+        return [{'prediction': names[key], 'prob': conf[i]} for i,key in enumerate(probs)]
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred during prediction: {str(e)}")
